b0b201b52ffd52f724484105755f7517b8fecc5e
Jorge A. Pérez <108704175+albertinho010@users.noreply.github.com>
2022-10-19 12:06:49 -0500

    mensajes

--- main.js
+++ main.js
@@ -4,13 +4,16 @@ console.log(Numero)
 
 if (isNaN (Numero)){
     console.log(' Este no es un numero')
+    alert('Este no es un número')
 }
 else{
 
 if(Numero%2==0){
     console.log('El ' +Numero+ ' es un número par')
+    alert('El ' +Numero+ ' es un número par')
 }
 else{
     console.log('El ' +Numero+ ' es un número impar')
+    alert('El ' +Numero+ ' es un número impar')
 }
 }
